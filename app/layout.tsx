602481ec6368ea1f94b1dedf5f531bb04056f1a0
sadique-mentoria <sadique.hussain@mentoria.com>
2026-02-23 18:42:02 +0530

    feat: Set default theme to dark mode by adding `dark` class to HTML.

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body suppressHydrationWarning>{children}</body>
     </html>
   );
